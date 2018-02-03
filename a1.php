ad5c40d0f0d40ab14dce7c1556dbbc631915b555
vipendra5151 <31948399+vipendra5151@users.noreply.github.com>
2018-02-03 13:43:50 +0530

    Update a1.php

--- a1.php
+++ a1.php
@@ -1,1 +1,12 @@
-vipendra pratap singh
+<?php
+if(isset($_GET['v1']) && isset($_GET['v2']) && isset($_GET['v3'])) {
+$volt=($_GET['v1'] * 256 + $_GET['v2']) * 5 / 1024;
+$volt=round($volt, 3);
+$a2="<!DOCTYPE html><br><html><br>" . $volt . " V<br>input=" . $_GET['v3'] . "<br><html>";
+$fp = fopen("a2.php", "w");
+fwrite($fp, $a2);
+fclose($fp);
+
+echo $volt . " V  input=" . $_GET['v3'];
+} 
+?>
